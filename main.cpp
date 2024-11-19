21f0da6ec0b5f6343d43b4fdd29ea7229fe851e4
oliverching <chingcc@connect.hku.hk>
2024-11-19 12:50:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include "character.h"
+#include "room.h"
+#include "maze.h"
 
+using namespace std;
+
+int main () {
+    cout << "Welcome to HKU mission impossible!" << endl;
+    
