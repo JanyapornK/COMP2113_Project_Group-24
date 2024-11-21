076f46f5e02811271797bb91f3b0387781880301
oliverching <chingcc@connect.hku.hk>
2024-11-21 15:47:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,43 @@
 
 using namespace std;
 
-int main () {
+char menu() 
+{
+    char choice;
+    //print the selection menu;
+    cout << "**********************************" << endl;
     cout << "Welcome to HKU mission impossible!" << endl;
+    cout << "**********************************" << endl;
+    cout << "1. New game." << endl;
+    cout << "2. Load game." << endl;
+    cout << "3. Exit." << endl;
+
+    cin >> choice;
+    cout << endl;
+
+    return choice;
+}
+
+int main() 
+{
+    char choice = menu();
+    while (choice != '3') 
+    {
+        switch (choice)
+        {
+            case '1':
+                //choose the character
+            case '2':
+                //load the previous saved game progress
+            default:
+                cout << "Invalid input!" << endl;
+        }
+        choice = meue();
+    }
+    cout << "Hope you enjoyed the game!" << endl;
+
+    return 0;
+}
+        
+    
     
