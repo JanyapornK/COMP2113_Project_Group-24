20f2ea0176e0e51a3084b2649c0d6b866db2489e
Lau Ting Hei Hayley <hayley.lau.t.h@gmail.com>
2024-11-26 11:50:09 +0800

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+struct Room{
+bool win;
+bool key;
+bool arrived;
+};
+
+int main(){
+struct Room canteen;
+struct Room lib;
+struct Room classrm;
+struct Room commonrm;
+
+srand((unsigned)time(NULL));
+int random=1+(rand()%3);
+
+
+return 0;
+}
