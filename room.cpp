1114c189193311a93315bcb8258a394e00b69e07
Lau Ting Hei Hayley <hayley.lau.t.h@gmail.com>
2024-11-28 11:04:25 +0800

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -4,22 +4,29 @@
 using namespace std;
 
 struct Room{
+string name;
 bool win;
 bool key;
 bool arrived;
 };
 
 int main(){
+string name[]={"canteen","lib","classrm","commonrm"};
 vector <Room> places;
-struct Room canteen;
-struct Room lib;
-struct Room classrm;
-struct Room commonrm;
-
 
 srand((unsigned)time(NULL));
-int random=(rand()%4);
+int random=(rand()%4); //random generate one room having key
 
+for (int i=0;i<name.size();i++){ //initialise rooms
+Room r;
+r.name=name[i];
+r.win=false;
+r.arrival=false;
+if (i==random){
+r.key=true;}else{
+r.key=false;}
+places.push_back(r);
+}
 
 return 0;
 }
