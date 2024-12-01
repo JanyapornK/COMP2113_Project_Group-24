7e949d4d4595b8cc351499a5947bd6e8e7a8137c
Lau Ting Hei Hayley <hayley.lau.t.h@gmail.com>
2024-12-01 16:10:06 +0800

    Update room.h

--- room.h
+++ room.h
@@ -1,1 +1,14 @@
 #ifndef ROOM_H
+#define ROOM_H
+
+#include <string>
+#include <vector>
+struct Room{
+  string name;
+  bool win;
+  bool key;
+  bool arrived; }
+
+void printChoices(vector <Room> places);
+
+#endif
