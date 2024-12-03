fe120cbbb55968d70c944825bb889202f5a29f8f
nck1217 <nck1217@connect.hku.hk>
2024-12-04 00:03:15 +0800

    Update battle.h

--- battle.h
+++ battle.h
@@ -2,12 +2,13 @@
 #ifndef BATTLE_H
 #define BATTLE_H
 
-int DICE_ROLLING();
-bool BATTLE_RESULT (Character& Player, Character& Opponent);
-void ATTACK (const int P_ATT, const int O_DEF, int& O_HP);
-void INT (const int P_INT, const int O_INT, int& P_HP, int& O_HP);
-void ATTACK_O (const int O_ATT, const int P_DEF, int& P_HP);
-void INT_O (const int O_INT, const int P_INT, int& O_HP, int& P_HP);
+#include "character.h"
 
+int DICE_ROLLING();
+void ATTACK(character* player, character& opponent);
+void ATTACK_O(character& opponent, character* player);
+void INT(character* player, character& opponent);
+void INT_O(character& opponent, character* player);
+bool BATTLE_RESULT (character* player, character& opponent);
 
 #endif
