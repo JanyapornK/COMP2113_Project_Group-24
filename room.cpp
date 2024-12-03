011c487bae8ae0eaa94856bb019a59e0602798e9
nck1217 <nck1217@connect.hku.hk>
2024-12-04 01:28:20 +0800

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -5,109 +5,173 @@
 #include <string>
 #include "room.h"
 #include "battle.h"
+#include "character.h"
+
 using namespace std;
 
 struct Room{
   string name;
   bool key;
   bool arrived; //t=user already got in before
+  character opponent; // Opponent in the room
 };
 
-void printChoices(vector <Room> places){
-  cout<<"Where are the keys...?"<<endl;
-  cout<<"Where would you go? (Enter the place)";
-  int count=0; //no of room not yet arrived
-  for (int i=0;i<places.size();i++){
-    if (places[i].arrived==false){
-      count+=1;}
+void printChoices(const vector<Room> places)
+{
+  cout << "Where are the keys...?" << endl;
+  cout << "Where would you go? (Enter the place)";
+  int count = 0; // no. of room not yet arrived
+  
+  for (int i = 0; i < places.size(); i++)
+  {
+    if (places[i].arrived == false)
+    {
+      count += 1;
+    }
   }
-  int counter=0;
-  //print places that not yet arrived
-  for (int i=0;i<places.size();i++){
-    if (places[i].arrived==false){
-      counter+=1;
-      if (counter != count){
-        cout<<places[i].name<<"/ ";
-      }else if (counter == count){
-      cout<<places[i].name<<endl;
+  
+  int counter = 0;
+  // Print places that not yet arrived
+  for (int i = 0; i < places.size(); i++)
+  {
+    if (places[i].arrived == false)
+    {
+      counter += 1;
+      if (counter != count)
+      {
+        cout << places[i].name << " / ";
+      }
+      else if (counter == count)
+      {
+      cout << places[i].name << endl;
       }
     }
   }
 }
 
-bool checkValidInput(string input, vector <Room> places){
+bool checkValidInput(const string& input, const vector<Room>& places)
+{
   string roomname;
-  for (Room r:places){
-    roomname=r.name;
+  for (Room r : places)
+  {
+    roomname = r.name;
     transform(roomname.begin(), roomname.end(), roomname.begin(), tolower); 
-    if (roomname==input && r.arrived==false){
+    if (roomname == input && r.arrived == false)
+    {
       return true;
     }
-
   }
   return false;
 }
 
-
-int findIndex(string input, string name[]){
-  string temp;
-  for (int i=0;i<4;i++){
-    temp=name[i];
-    transform(temp.begin(), temp.end(), temp.begin(), tolower); 
-    if (temp == input){
-      return i;
-    }
-  }
-}
-
-
-int Rmain(character *playerCharacter, character opponent){
-  string name[]={"Canteen","Library","Classroom","Common room"};
-  vector <Room> places; 
+int Rmain(character *playerCharacter){
+  vector<string> name = { "Canteen", "Library", "Classroom", "Common room" };
+  vector<Room> places; 
 
   srand((unsigned)time(NULL));
-  int random=(rand()%4); //random generate one room having key
+  int random = rand() % 4; // Random generate one room having key
 
-  for (int i=0;i<4;i++){ //initialise rooms
+  for (int i = 0; i < 4; i++)
+  { 
+    // Initialise rooms
     Room r;
-    r.name=name[i];
-    r.arrived=false;
-    if (i==random){
-      r.key=true;}
-    else{
-      r.key=false;}
+    r.name = name[i];
+    r.arrived = false;
+    r.key = (i == random);
+
+    // Initialising rooms' opponents
+    int oppHealth = 25;
+    int oppAttack = rand() % 10 + 1;
+    int oppDefence = rand() % 10 + 1;
+    int oppIntelligence = rand() % 10 + 1;
+    // Ensuring that room minibosses have only one stat above 5
+    int statsAboveFive = 0;
+    if (oppAttack > 5) statsAboveFive++;
+    if (oppDefence > 5) statsAboveFive++;
+    if (oppIntelligence > 5) statsAboveFive++;
+    // Adjusting stats to meet the above requirement
+    while (statsAboveFive > 1)
+      {
+        if (oppAttack > 5)
+        {
+          oppAttack = rand() % 5 + 1;
+          statsAboveFive--;
+        }
+        if (statsAboveFive <= 1) break;
+        if (oppDefence > 5)
+        {
+          oppDefence = rand() % 5 + 1;
+          statsAboveFive--;
+        }
+        if (statsAboveFive <= 1) break;
+        if (oppIntelligence > 5)
+        {
+          oppIntelligence = rand() % 5 + 1;
+          statsAboveFive--;
+        }
+      }
+    r.opponent = character("Guard", oppHealth, oppAttack, oppIntelligence);
+    
     places.push_back(r);
   }
 
-bool gameover = false;
-while (gameover==false){
-  printChoices(places); //print rooms that is unarrived by user
-  string input;
-  cin>>input;
-  transform(input.begin(), input.end(), input.begin(), tolower); 
-  
-  if (checkValidInput(input,places)==false){
-    cout<<"The place is invalid. Choose another place to go... (Re-enter a place)" <<endl; 
-    continue;//rechoose
-  }
-
-  int roomIndex = findIndex(input,name);
-  bool result = BATTLE_RESULT(*playerCharacter, opponent);
-  
-  if (result == true && places[roomIndex].key == true){
-        cout<<"You defeated the opponent, and the key is here!"<<endl;
-        break;}
-      else if (result == true && places[roomIndex].key == false){ 
-        places[roomIndex].arrived = true;
-        cout<<"You defeated the opponent, but the key is not here. Maybe it is in the other room."<<endl;
-        continue;}
-      else if (result == false){ 
-        gameover=true;
-        cout<<"You were defeated! (GAME OVER)"<<endl;
-        break;}
-
-
-
+  bool gameover = false;
+  while (!gameover)
+  {
+    printChoices(places); // Print unvisited rooms
+    string input;
+    getline(cin >> ws, input);
+    transform(input.begin(), input.end(), input.begin(), tolower); 
     
-return gameover ? 1 : 0; // Return 1 if gameover
+    if (!checkValidInput(input, places))
+    {
+      cout << "The place is invalid or already visited, Choose another place to go... (Re-enter a place)" << endl; 
+    }
+    else
+    {
+      int roomIndex = -1;
+      for (int i = 0; i < places.size(); i++)
+      {
+        string roomname = places[i].name;
+        transform(roomname.begin(), roomname.end(), roomname.begin(), tolower());
+        if (roomname == input)
+        {
+          roomIndex = i;
+          break;
+        }
+      }
+  
+      if (roomIndex == -1)
+      {
+        cout << "An error occured. Please try again." << endl;
+        continue;
+      }
+      Room& currentRoom = places[roomIndex];
+  
+      // Start battle with the opponent in the room
+      bool result = BATTLE_RESULT(playerCharacter, currentRoom.opponent);
+    
+      if (result) // Player won the battle
+      {
+        currentRoom.arrived = true;
+        if (currentRoom.key) // Key is in current room
+        {
+          cout << "You defeated the opponent, and the key is here!" << endl;
+          break;
+        }
+        else
+        {
+          cout << "You defeated the opponent, but the key is not here. Maybe it is in the other room." << endl;
+        }
+      }
+      else // Player lost the battle
+      {
+        gameover = true;
+        cout << "You were defeated! (GAME OVER)" << endl;
+        break;
+      }
+    }
+  }
+  
+  return gameover ? 1 : 0; // Return 1 if gameover, 0 if key is found
 }
