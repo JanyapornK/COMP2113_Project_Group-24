01b2f024e99c5f4e54c74948cab4c42af74e7dbd
wlchu912 <155963310+wlchu912@users.noreply.github.com>
2024-12-03 12:54:47 +0800

    Update battle.cpp

--- battle.cpp
+++ battle.cpp
@@ -1,1 +1,12 @@
+/* For this to work, it is expected that the program will receive 4 types of data from a struct (the character stuff).
+i.e.
+struct Character {
+  int HP;
+  int ATT;
+  int DEF;
+  int INT;
+ They will be ranging from 1-10, HP will be 25 for the mini bosses, 50 for the prof
+}
 
+
+*/
