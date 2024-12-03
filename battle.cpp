13c0217e74446f7d28c0c35810601b8f34acb506
wlchu912 <155963310+wlchu912@users.noreply.github.com>
2024-12-03 16:17:01 +0800

    Update battle.cpp

--- battle.cpp
+++ battle.cpp
@@ -1,12 +1,28 @@
-/* For this to work, it is expected that the program will receive 4 types of data from a struct (the character stuff).
-i.e.
+/* For this to work, the program is expected to receive 4 types of data from a struct (the character stuff).
+e.g.
 struct Character {
-  int HP;
+  int HP/Stamina;
   int ATT;
   int DEF;
   int INT;
- They will be ranging from 1-10, HP will be 25 for the mini bosses, 50 for the prof
 }
+ATT, DEF, and INT will range from 1 to 10 (randomly generated for opponents, with minibosses having only 1 of them above five and the prof having 2 of them above 5. )
+HP/Stamina will be 25 for the mini-bosses and 50 for the professor.No regeneration of HP, based on items and stuff after 
+The idea is as follows: suppose the player has the following stats: HP = 50, ATT = 6, DEF = 5, INT = 7, the prof has HP = 50, Att = 7, Def = 7, Int = 9
+(The intelligence of the players determines the possibility of avoiding battle at all, while the Intelligence of the bosses determine playstyle)
+The battle mech is a turned-based attack. Both the bosses and the player have 2 choices: attack or heal/intimidate.
+Attack:
+1. Damage = dice throw (1-6) + (ATT - DEF)
+- If ends up less than 0, damage failed,
+- regardless it is the opponents turn, same process
 
-
+Heal/Intimidate:
+if player Int > opponent:
+  -> roll dice (1-6)
+  -> (SPECIAL) -> if = = 6 win autonomatically
+  Number rolled += player's HP
+if player Int < = opponent:
+  Number rolled += player's HP
 */
+
+
