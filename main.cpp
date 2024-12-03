f62e5e0386e98c822235a6a40b0f7f018b7daad3
oliverching <chingcc@connect.hku.hk>
2024-12-03 23:31:22 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,8 +17,9 @@ char menu()
     cout << "Welcome to HKU mission impossible!" << endl;
     cout << "**********************************" << endl;
     cout << "1. New game." << endl;
-    cout << "2. Load game." << endl;
-    cout << "3. Exit." << endl;
+    cout << "2. Save game." << endl;
+    cout << "3. Load game." << endl;
+    cout << "4. Exit." << endl;
 
     cin >> choice;
     cout << endl;
@@ -26,7 +27,7 @@ char menu()
     return choice;
 }
 
-int backgroundStory() {
+void backgroundStory() {
     cout << "A rumor spreads through the campus about the professor losing his office key." << endl;
     this_thread::sleep_for(chromo::seconds(1)); // Delay for 1 second
     cout << "Whispers suggest that finding the key grants access to exam papers for better results." << endl;
@@ -43,43 +44,65 @@ void startNewGame() {
     
     // Choose the character
     character* playerCharacter = character::selectCharacter();
+    int currentScenario = 1; // Set initial scenario
 
     if (playerCharacter) {
         cout << "You have selected the following character: " << endl;
         playerCharacter->displayStats();
 
-        // Go to room.cpp to find the key
-        character opponent();
-        Rmain(*playerCharacter, opponent);
-        
-        // Scenario 1: Solve the maze to enter the professor's office
-        cout << "You must solve the maze to enter the professor's office!" << endl;
-        Maze mazeEnter(1);
-        mazeEnter.play();
+        // Start the gameplay loop
+        bool playing = true;
+        while (playing) {
+            cout << "You are in a new scenario. What would you like to do?" << endl;
+            cout << "1. Exlopre the maze." << endl;
+            cout << "2. Save game." << endl;
+            cout << "3. Exit to the main menu." << endl;
 
-        // Scenario 2: Solve the maze to escape after stealing the exam paper
-        cout << "You must solve the maze to escape the building!" << endl;
-        Maze mazeOut(2);
-        mazeOut.play();
+            char choice;
+            in >> choice;
 
-        // Battle system
-        character opponent();
-        if (BATTLE_RESULT(*playerCharacter, opponent)) {
-            cout << "You defeated the opponent!" << endl;
+            switch (choice) {
+                case '1':
+                    cout << "You must solve the maze to enter the professor's office!" << endl;
+                    Maze mazeEnter(currentScenario);
+                    mazeEnter.play();
+                    break;
+
+                case '2':
+                    saveGame(playerCharacter, currentScenario);
+                    break;
+
+                case '3':
+                    playing = false;
+                    break;
+
+                default:
+                    cout << "Invalid input! Please select 1, 2, or 3." << endl;
+                    break;
+            }
         }
-            
-        else {
-            cout << "You were defeated!" << endl;
-        }
-
         delete playerCharacter; // Clean up dynamically allocated memory
-    }
-        
+    }        
     else {
         cout << "Error selecting character." << endl;
     }
 }
+        // Go to room.cpp to find the key
+        //character opponent();
+        //Rmain(*playerCharacter, opponent);
+        
+        // Scenario 1: Solve the maze to enter the professor's office
+        //cout << "You must solve the maze to enter the professor's office!" << endl;
+        //Maze mazeEnter(1);
+        //mazeEnter.play();
 
+        // Scenario 2: Solve the maze to escape after stealing the exam paper
+        //cout << "You must solve the maze to escape the building!" << endl;
+        //Maze mazeOut(2);
+        //mazeOut.play();
+
+        // Battle system
+        //character opponent();
 void loadGame() {
     ifstream infile("savegame.txt");
     
@@ -99,16 +122,30 @@ void loadGame() {
     Maze maze(currentScenario);
     maze.play(); // Enter the maze gameplay
 
-    // Clean up
-    delete playerCharacter;
+    delete playerCharacter; // Clean up dynamically allocated memory
+}       
+        
+void saveGame(character* playerCharacter, int currentScenario) {
+    ofstream outfile("savegame.txt");
+    if (outfile) {
+        coutfile << playerCharacter->getName() << " "
+                 << playerCharacter->getHealth() << " "
+                 << playerCharacter->getMaxHealth() << " "
+                 << playerCharacter->getAttack() << " "
+                 << playerCharacter->getDefence() << " "
+                 << playerCharacter->getIntelligence() << " "
+                 << currentScenario << endl;
+        cout << "Game saved successfully!" << endl;
+    }
+    else {
+        cout << "Error in saving the game." << endl;
+    }
 }
-                    
-                
+                             
 int main() 
 {
     char choice = menu();
-    
-    while (choice != '3') 
+    while (choice != '4') 
     {
         switch (choice)
         {
@@ -117,17 +154,19 @@ int main()
                 break;
             
             case '2':
+                saveGame(); // Save the game progress
+                break;
+
+            case '3':
                 loadGame(); // Load the previous saved game progress
                 break;
             
             default:
                 cout << "Invalid input! Please select 1, 2, or 3." << endl;
                 break;
-        }
-        
+        }        
         choice = menu(); // Show the menu again
-    }
-    
+    }    
     cout << "Hope you enjoyed the game!" << endl;
 
     return 0;
