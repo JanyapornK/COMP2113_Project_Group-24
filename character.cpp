c4a3fed828fb636129a6f108ea428e40c25c2343
nck1217 <nck1217@connect.hku.hk>
2024-12-03 21:47:19 +0800

    Update character.cpp

--- character.cpp
+++ character.cpp
@@ -2,32 +2,30 @@
 #include <iostream>
 #include <string>
 
-// Constructor
+// Base class constructor
 character::character() : name(""), health(100), attack(10), defence(5), intelligence(0) {}
-
-character::character(std::string name, int health, int attack, int defence, int intelligence) : name(name), health(health), attack(attack), defence(defence), intelligence(intelligence) {}
-// Destructor
+character::character(std::string name, int health, int attack, int defence, int intelligence, std::string specialAbility) : name(name), health(health), attack(attack), defence(defence), intelligence(intelligence), specialAbility(specialAbility) {}
+// Base class destructor
 character::~character() {}
 
 // Stat getting methods
 std::string character::getName() const { return name; }
 int character::getHealth() const { return health; }
-int character::getMaxHealth() const { return maxHealth; }
 int character::getDefence() const { return defence; }
 int character::getIntelligence() const { return intelligence; }
-// string getSpecialAbility() const { return specialAbility; } (TBD)
+std::string character::getSpecialAbility() const { return specialAbility; }
 
 // Stat setting method
 void character::setHealth(int h)
 {
-	health = (h > maxHealth) ? maxHealth : h;
+	health = h;
 }
 
 /*
-// virtual method for speical ability
+// Virtual method for speical ability
 void character::useSpecialAbility()
 {
-	cout << name << " uses " << specialAbility << "!" << endl;
+	std::cout << name << " uses " << specialAbility << "!" << std::endl;
 	// ability effect in battle classes
 }
 */
@@ -35,12 +33,12 @@ void character::useSpecialAbility()
 // Stat displaying method
 void character::displayStats() const
 {
-	std::cout << "Character: " << name << std::endl;
-	std::cout << "Health: " << health << "/" << maxHealth << std::endl;
+	std::cout << "\nCharacter: " << name << std::endl;
+	std::cout << "Health: " << health << std::endl;
 	std::cout << "Attack: " << attack << std::endl;
 	std::cout << "Defence: " << defence << std::endl;
 	std::cout << "Intelligence: " << intelligence << std::endl;
-	// cout << "Special Ability: " << specialAbility << endl; (TBD)
+	std::cout << "Special Ability: " << specialAbility << std::endl;
 }
 
 // Character selection method
@@ -48,10 +46,9 @@ character* character::selectCharacter()
 {
 	int choice;
 	std::cout << "Choose your character: " << std::endl;
-	std::cout << "1: ???" << std::endl; // characters making still in progress
-	std::cout << "2: ???" << std::endl;
-	std::cout << "3: ???" << std::endl;
-	std::cout << "4: ???" << std::endl;
+	std::cout << "1: Athlete" << std::endl; // characters making still in progress
+	std::cout << "2: Hacker" << std::endl;
+	std::cout << "3: Rebel" << std::endl;
 	std::cout << "Enter the number of your choice: ";
 	std::cin >> choice;
 
@@ -60,23 +57,51 @@ character* character::selectCharacter()
 	switch (choice)
 	{
 	case 1:
-		player = new character("???", 150, 20, 15, 5);
+		player = new athlete();
 		break;
 	case 2:
-		player = new character("???", 100, 10, 5, 25);
+		player = new hacker();
 		break;
 	case 3:
-		player = new character("???", 120, 15, 10, 10);
-		break;
-	case 4:
-		player = new character("???", 110, 18, 8, 8);
+		player = new rebel();
 		break;
 	default:
-		std::cout << "Invalid choice. Defaulting to ???." << std::endl; // by default choosing the 1st character
-		player = new character("???", 150, 20, 15, 5);
+		std::cout << "Invalid choice. Defaulting to Athlete." << std::endl; // by default choosing the 1st character
+		player = new athlete();
 		break;
 	}
 
 	player->displayStats();
 	return player;
+}
+
+// ===============
+// Derived classes
+// ===============
+
+// Athlete class
+athlete::athlete()
+	: character("Athelete", 150, 25, 20, 5, "Smash") {}
+void athlete::useSpecialAbility()
+{
+	std::cout << name << " uses " << specialAbility << "!" << std::endl;
+	// Implement effect: have a chance to deal double damage
+}
+
+// Hacker class
+hacker::hacker()
+	: character("Hacker", 100, 15, 15, 10, "Override") {}
+void hacker::useSpecialAbility()
+{
+	std::cout << name << " uses " << specialAbility << "!" << std::endl;
+	// Implement effect: have a chance to skip battle in rooms
+}
+
+// Rebel class
+rebel::rebel()
+	: character("Rebel", 75, 10, 10, 5, "Pick Lock") {}
+void rebel::useSpecialAbility()
+{
+	std::cout << name << " uses " << specialAbility << "!" << std::endl;
+	// Implement effect: entering office without the need of a key
 }
