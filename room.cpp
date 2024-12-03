cbe4b80f1174902cc1784938caddb78ed810c5e2
Lau Ting Hei Hayley <hayley.lau.t.h@gmail.com>
2024-12-04 00:49:49 +0800

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -48,6 +48,19 @@ bool checkValidInput(string input, vector <Room> places){
   return false;
 }
 
+
+int findIndex(string input, string name[]){
+  string temp;
+  for (int i=0;i<4;i++){
+    temp=name[i];
+    transform(temp.begin(), temp.end(), temp.begin(), tolower); 
+    if (temp == input){
+      return i;
+    }
+  }
+}
+
+
 int Rmain(character *playerCharacter, character opponent){
   string name[]={"Canteen","Library","Classroom","Common room"};
   vector <Room> places; 
@@ -74,64 +87,27 @@ while (gameover==false){
   transform(input.begin(), input.end(), input.begin(), tolower); 
   
   if (checkValidInput(input,places)==false){
-    cout<<"The place is invalid. Choose another place to go... (Re-enter a place)" <<endl; //rechoose
-  }else{
- 
-    if (input=="canteen"){
-      bool result = BATTLE_RESULT(*playerCharacter, opponent);
-      if (result = true && places[0].key == true){
-        cout<<"You defeated the opponent, and the key is here!"<<endl;
-        break;}
-      else if (result = true && places[0].key == false){ 
-        places[0].arrived = true;
-        cout<<"You defeated the opponent, but the key is not here. Maybe it is in the other room."<<endl;
-        continue;}
-      else if (result = false){ 
-        gameover=true;
-        cout<<"You were defeated! (GAME OVER)"<<endl;
-        break;}
-    } else if (input=="library"){
-      bool result = BATTLE_RESULT(*playerCharacter, opponent);
-      if (result = true && places[1].key == true){
-        cout<<"You defeated the opponent, and the key is here!"<<endl;
-        break;}
-      else if (result = true && places[1].key == false){ 
-        places[0].arrived = true;
-        cout<<"You defeated the opponent, but the key is not here. Maybe it is in the other room."<<endl;
-        continue;}
-      else if (result = false){ 
-        gameover=true;
-        cout<<"You were defeated! (GAME OVER)"<<endl;
-        break;}
-    }else if (input=="classroom"){
-      bool result = BATTLE_RESULT(*playerCharacter, opponent);
-      if (result = true && places[2].key == true){
-        cout<<"You defeated the opponent, and the key is here!"<<endl;
-        break;}
-      else if (result = true && places[2].key == false){ 
-        places[0].arrived = true;
-        cout<<"You defeated the opponent, but the key is not here. Maybe it is in the other room."<<endl;
-        continue;}
-      else if (result = false){ 
-        gameover=true;
-        cout<<"You were defeated! (GAME OVER)"<<endl;
-        break;}
-    }else if (input=="common room"){
-      bool result = BATTLE_RESULT(*playerCharacter, opponent);
-      if (result = true && places[3].key == true){
-        cout<<"You defeated the opponent, and the key is here!"<<endl;
-        break;}
-      else if (result = true && places[3].key == false){ 
-        places[0].arrived = true;
-        cout<<"You defeated the opponent, but the key is not here. Maybe it is in the other room."<<endl;
-        continue;}
-      else if (result = false){ 
-        gameover=true;
-        cout<<"You were defeated! (GAME OVER)"<<endl;
-        break;}
-    }
+    cout<<"The place is invalid. Choose another place to go... (Re-enter a place)" <<endl; 
+    continue;//rechoose
+  }
 
-}
+  int roomIndex = findIndex(input,name);
+  bool result = BATTLE_RESULT(*playerCharacter, opponent);
+  
+  if (result == true && places[roomIndex].key == true){
+        cout<<"You defeated the opponent, and the key is here!"<<endl;
+        break;}
+      else if (result == true && places[roomIndex].key == false){ 
+        places[roomIndex].arrived = true;
+        cout<<"You defeated the opponent, but the key is not here. Maybe it is in the other room."<<endl;
+        continue;}
+      else if (result == false){ 
+        gameover=true;
+        cout<<"You were defeated! (GAME OVER)"<<endl;
+        break;}
+
+
+
     
 return gameover ? 1 : 0; // Return 1 if gameover
 }
