74d02c6dfefefc8bda1a1955d9961e7353460a98
oliverching <chingcc@connect.hku.hk>
2024-12-03 21:15:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ int backgroundStory() {
     this_thread::sleep_for(chromo::seconds(1)); // Delay for 1 second
     cout << "Whispers suggest that finding the key grants access to exam papers for better results." << endl;
     this_thread::sleep_for(chromo::seconds(1)); // Delay for 1 second
-    cout << "Students are tempted by the opportunity, and embark on a clandestine quest to locate the missing key." << endl;
+    cout << "Students are tempted by the opportunity and embark on a clandestine quest to locate the missing key." << endl;
     this_thread::sleep_for(chromo::seconds(1)); // Delay for 1 second
     cout << "Now hurry up and be the first person to find the key!!!" << endl;
     return 0;
